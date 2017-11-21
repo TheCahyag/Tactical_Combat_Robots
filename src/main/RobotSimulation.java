2fe965b8ed5b8bdfbbed39754c6aa8a8fe6f17b3
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-11-21 03:03:41 -0500

    Basic program creation

--- src/main/RobotSimulation.java
+++ src/main/RobotSimulation.java
@@ -1,9 +1,23 @@
 package main;
 
+import grid.Grid;
+import inhabitant.BenignRobot;
+import inhabitant.Inhabitant;
+
 /**
  * File: main.RobotSimulation.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class RobotSimulation {
+    private static Grid grid;
+
+    public static void main(String[] args) {
+        Grid grid = new Grid(10);
+        RobotSimulation.grid = grid;
+        grid.init(3);
+        System.out.println(grid);
+        ((BenignRobot) grid.inhabitants.get(0)).addMove(Inhabitant.Direction.NORTH);
+        System.out.println(grid);
+    }
 }
