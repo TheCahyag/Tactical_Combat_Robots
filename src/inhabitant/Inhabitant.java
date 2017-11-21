71f4ee514400c9ed6679be5736f7d5fda0483621
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-11-21 03:00:31 -0500

    Added getters, setters, constructor, and a Direction Enum

--- src/inhabitant/Inhabitant.java
+++ src/inhabitant/Inhabitant.java
@@ -1,6 +1,7 @@
 package inhabitant;
 
 import grid.Grid;
+import grid.Location;
 
 /**
  * File: inhabitant.Inhabitant.java
@@ -9,18 +10,66 @@ import grid.Grid;
  */
 public abstract class Inhabitant implements Runnable {
     private Grid grid;
-    private String name;
-    private int posX;
-    private int posY;
+    private Location location;
     private int unitsMoved = 0;
-    private Mode Status;
+    private Mode status;
 
+    /**
+     * Do nothing constructor does nothing
+     */
+    public Inhabitant(){}
+
+    public Inhabitant(Grid grid, Location location){
+        this.grid = grid;
+        this.location = location;
+    }
+
+    /**
+     * Mode defines what the inhabitant is currently doing.
+     * SEARCHING - Searching for the target, if the inhabitant is the target it is defaulted to IN_POSITION
+     */
     public static enum Mode {
         SEARCHING,
         PURSUING,
         IN_POSITION
     }
 
+    /**
+     * Cardinal directions
+     */
+    public static enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
     @Override
     public abstract void run();
+
+    /**
+     *
+     * @return
+     */
+    public abstract boolean isTarget();
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Mode getStatus() {
+        return status;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setStatus(Mode status) {
+        this.status = status;
+    }
 }
