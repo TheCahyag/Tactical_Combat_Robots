6d7350433f4a0ebec9cbaf3277dd87e31562d54f
Brandon Bires-Navel <brn5915@rit.edu>
2017-11-22 17:07:38 -0500

    Created class that will contain methods to determine shortest paths and search algorithms

---
+++ src/grid/PathFinding.java
@@ -0,0 +1,23 @@
+package grid;
+
+import inhabitant.Inhabitant;
+
+/**
+ * File: PathFinding.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class PathFinding {
+
+    /**
+     * Finds the shorts path from one location to another and
+     * returns the list of moves to get to that position
+     * @param start starting {@link Location}
+     * @param end desired {@link Location}
+     * @return Array of Directions that server as moves to get from the start
+     * location to the end location, where Directions[0] is the first move to take
+     */
+    public static Inhabitant.Direction[] shortestPath(Location start, Location end){
+        return null;
+    }
+}
