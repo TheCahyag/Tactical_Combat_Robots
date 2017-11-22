64713824f7a9c1ec0042da3b241a6ded88411142
Brandon Bires-Navel <brn5915@rit.edu>
2017-11-22 16:04:21 -0500

    Created classes containing the possible names for Robots and Targets

---
+++ src/resource/RobotNames.java
@@ -0,0 +1,34 @@
+package resource;
+
+/**
+ * File: RobotNames.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class RobotNames {
+    public static String[] ROBOT_NAMES = {
+            "Superman",
+            "Batman",
+            "Spider-Man",
+            "Thor",
+            "Wonder Woman",
+            "Captain America",
+            "Thing",
+            "Mr. Fantastic",
+            "Wolverine",
+            "Iron Man",
+            "Daredevil",
+            "Hulk",
+            "Aquaman",
+            "Hercules",
+            "Black Panther",
+            "Mr. Incredible",
+            "Mrs. Incredible",
+            "Doctor Strange",
+            "Hawkeye",
+            "Nick Fury",
+            "Black Widow",
+            "Luke Cage",
+            "Deadpool"
+    };
+}
---
+++ src/resource/TargetNames.java
@@ -0,0 +1,26 @@
+package resource;
+
+/**
+ * File: TargetNames.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class TargetNames {
+    public final static String[] TARGET_NAMES = {
+            "Bane",
+            "Darth Vader",
+            "Magneto",
+            "Loki",
+            "Darth Maul",
+            "T1000",
+            "Sauron",
+            "Predator",
+            "Maleficent",
+            "Cruella de Vil",
+            "Killer Croc",
+            "Dr. Otto Octavius",
+            "Doctor Doom",
+            "Stepmother",
+            "Ursula"
+    };
+}
