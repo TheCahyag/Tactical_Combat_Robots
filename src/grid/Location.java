21384750a78473fc67c500dfe26f99bc803cfb5b
Brandon Bires-Navel <brn5915@rit.edu>
2017-11-23 09:23:24 -0500

    Implemented toString

--- src/grid/Location.java
+++ src/grid/Location.java
@@ -29,4 +29,9 @@ public class Location {
     public void setY(int y) {
         this.y = y;
     }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
